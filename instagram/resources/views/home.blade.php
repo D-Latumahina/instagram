f6b9c962139ac6411d74f01d2552d5bbc31af541
D-Latumahina <danifct65@gmail.com>
2023-03-14 14:37:41 +0100

    Updated User Register
    added username to register

--- instagram/resources/views/home.blade.php
+++ instagram/resources/views/home.blade.php
@@ -2,21 +2,35 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
+    <div class="row p-4">
+            <div class="col-3">
+            ㅤㅤㅤ <img src="https://avatarfiles.alphacoders.com/240/240822.jpg" class="rounded-circle" style="height: 16%">
             </div>
+
+        <div class="col-9 ">
+            <div>
+                <h2>{{ Auth::user()->username }}</h2>
+            </div>
+
+        <div class="d-flex">
+            <div class="pr-5"><strong>153 </strong> postsㅤㅤ</div>
+            <div class="pr-5"><strong>23K </strong> followersㅤㅤ</div>
+            <div class="pr-5"><strong>100 </strong> following</div>
+        </div>
+
+        <div class="pt-4 font-weight-bold"><strong> Instagram.com </strong></div>  
+        <div>Discover what's next on Instagram 🔎✨</div>
+        <div><a href="#">www.instagram.com</div></a>
+        <hr class="border border-default border-1 opacity-100">
+    <div class="row pt-4">
+        <div class="col-4">
+            <img src="https://avatarfiles.alphacoders.com/240/240822.jpg" class="w-100">
+        </div>
+        <div class="col-4">
+            <img src="https://avatarfiles.alphacoders.com/240/240822.jpg" class="w-100">
+        </div>
+        <div class="col-4">
+            <img src="https://avatarfiles.alphacoders.com/240/240822.jpg" class="w-100">
         </div>
     </div>
 </div>
